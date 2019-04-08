b3c72608b563f44d548b241c95316dd39ed698fb
Derick Zhang <derickzhang@fb.com>
2019-04-07 17:28:08 -0700

    Description

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <h1>Welcome to mikey Troll FACE, home of the Troll Face!</h1>
 	<img src="http://orig07.deviantart.net/41a2/f/2010/189/d/f/trollface_by_deniskapwnz.png" height = "500" width= "500">
     <br/>
-	<a href="sia.mp3"> hi </a>
+	<a href="sia.mp3">click here for awesome music!</a>
 	
  </body>
 
