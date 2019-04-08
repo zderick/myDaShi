8270a1c7b64117db7f6a8ecea5fc0a47e80d3c0c
Derick Zhang <derickzhang@fb.com>
2019-04-07 19:46:56 -0700

    Add game

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
     
 <body>
     
-    <h1>Welcome to mikey Troll FACE, home of the Troll Face!</h1>
+    <h1>Welcome to my website: Home of my DaShi!</h1>
 	<img src="http://orig07.deviantart.net/41a2/f/2010/189/d/f/trollface_by_deniskapwnz.png" height = "500" width= "500">
     <br/>
 	<a href="sia.mp3">click here for awesome music!</a>
+	<a href="clonewars.html">Click here for game</a>
 	
  </body>
 
