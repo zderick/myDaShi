4c7128453d91c3b58c037576465c1e220d87ecc9
Derick Zhang <derickzhang@fb.com>
2019-04-07 20:37:30 -0700

    Change picture

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
     
     <h1>Welcome to my website: Home of my DaShi!</h1>
-	<img src="http://orig07.deviantart.net/41a2/f/2010/189/d/f/trollface_by_deniskapwnz.png" height = "500" width= "500">
+	<img src="https://lh3.googleusercontent.com/cjNjd6_HIdVuTHL9dbimH7-Z85977U1sx2ZlQ5xLw5BZ51Xq23e4DwzuLRhO_M4Q4dGo5I2RGfaJfTWTipTz5AtUnWvwfkSHt8CKr5EvyWRRi7r1UNHtceybvrcNJf_P1m7kqfTHe4t443Lb5Bd1vHyAz2HXz792D4eLSsLnZ25tScBeuxY97RlPyEp9BH9VVSZ903mJZivjw56r-4R6ZinjxHBKwvoWZIxOpDpA-RNCkGMnEHSvaimaLXyrUJOg_6hq8yqsiXUKATgExhCBQ9T5XjZ8_fswoqilnzoEPGBnJ6J_NKVsG_rGJcD14tdL7Uta_MoLy26j9ZWci_AO646-niepBshoO1uzWliS3ZTBBNlAdf71yjjC1hMdQyPlQDLUo1-WhVGwKeoP8yjsX5kyIwOmKKR-43X6glfYPICLf1kqEQS8KuvBbuI3jzC73kpTKaOpJw3GPkGG3CaXJKNacrJoI5O9aLAEqmdKhM_oSg0uQRmLNA9L0osrMEZh8KV8bMwEDDKFW2yhj0zCYJRlp-2T927Jjg-ICbx6jdUQgQWhwIfj2wBovzR1w1bXDEAypQy_3R3_uJaJs2aPSHfxtKU9Kv0jkvsRyx60TZrsMIBajTkIrRc9DDhY32l1Crks8XDf4GjyqxpuPf_cJrLgJXvNp3fo=w304-h228-no" height = "500" width= "500">
     <br/>
 	<a href="sia.mp3">click here for awesome music!</a>
 	<a href="clonewars.html">Click here for game</a>
@@ -22,3 +22,26 @@
 
 
 
+$db = parse_url(getenv("https://data.heroku.com/datastores/0dfa2da9-851e-450a-a7fa-7655078f6612"));
+$db["path"] = ltrim($db["path"], "/");
+
+
+
+
+  -h, --host=HOSTNAME      database server host or socket directory (default: "local socket")
+  -p, --port=PORT          database server port (default: "5432")
+  -U, --username=USERNAME  database user name (default: "derickzhang")
+  -w, --no-password        never prompt for password
+  -W, --password           force password prompt (should happen automatically)
+
+
+
+CREATE TABLE IF NOT EXISTS test(
+	da_shi_id int NOT NULL,
+	my_str VARCHAR,
+	PRIMARY KEY(da_shi_id)
+);
+
+
+INSERT INTO test(my_str, da_shi_id)
+VALUES ('numberone!', 1);
