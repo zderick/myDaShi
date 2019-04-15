11e0fd6cac2f40713698ad77025143d86c2aa55f
zderick <centuryib100@gmail.com>
2019-04-14 17:28:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
 <body>
     
     <h1>Welcome to my website: Home of my DaShi!</h1>
-	<img src="https://lh3.googleusercontent.com/cjNjd6_HIdVuTHL9dbimH7-Z85977U1sx2ZlQ5xLw5BZ51Xq23e4DwzuLRhO_M4Q4dGo5I2RGfaJfTWTipTz5AtUnWvwfkSHt8CKr5EvyWRRi7r1UNHtceybvrcNJf_P1m7kqfTHe4t443Lb5Bd1vHyAz2HXz792D4eLSsLnZ25tScBeuxY97RlPyEp9BH9VVSZ903mJZivjw56r-4R6ZinjxHBKwvoWZIxOpDpA-RNCkGMnEHSvaimaLXyrUJOg_6hq8yqsiXUKATgExhCBQ9T5XjZ8_fswoqilnzoEPGBnJ6J_NKVsG_rGJcD14tdL7Uta_MoLy26j9ZWci_AO646-niepBshoO1uzWliS3ZTBBNlAdf71yjjC1hMdQyPlQDLUo1-WhVGwKeoP8yjsX5kyIwOmKKR-43X6glfYPICLf1kqEQS8KuvBbuI3jzC73kpTKaOpJw3GPkGG3CaXJKNacrJoI5O9aLAEqmdKhM_oSg0uQRmLNA9L0osrMEZh8KV8bMwEDDKFW2yhj0zCYJRlp-2T927Jjg-ICbx6jdUQgQWhwIfj2wBovzR1w1bXDEAypQy_3R3_uJaJs2aPSHfxtKU9Kv0jkvsRyx60TZrsMIBajTkIrRc9DDhY32l1Crks8XDf4GjyqxpuPf_cJrLgJXvNp3fo=w304-h228-no" height = "500" width= "500">
-    <br/>
+	<img src="https://insideretail.asia/wp-content/uploads/2018/07/Detective-Conan.jpg" alt="Picture"/>
+	<br/>
 	<a href="sia.mp3">click here for awesome music!</a>
 	<br/>
 	<a href="clonewars.html">Click here for game</a>
